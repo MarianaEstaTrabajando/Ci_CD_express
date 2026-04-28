908be40ebf3ccd1888bed0524b21102ce47ee9a3
Marian_Corps <nyashiko@gmail.com>
2026-04-28 11:23:31 -0600

    Ejecutar pruebas en main.yml

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Esta es una respuesta de la API')
+    res.send('Esta es una respuesta sencilla')
 });
 
 
