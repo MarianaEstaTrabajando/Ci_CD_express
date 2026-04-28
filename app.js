174467982d00a7adad6128ea014057e0af48629c
Marian_Corps <nyashiko@gmail.com>
2026-04-28 11:07:45 -0600

    biblioteca jester y package.json jester

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ app.get('/', (req, res) => {
     res.send('Esta es una respuesta de la API')
 });
 
+
+//http://localhost:3000/suma?a=4&b=7
+
 app.get('/suma', (req, res) => {
-    const {a,b} = req.query;
-    const resultado= Number(a) + Number(b);
-    res.json({resultado});
+    const {a, b} = req.query;
+    const resultado = Number(a) + Number(b);
+    res.json("resultado"+{resultado});
 });
 
 module.exports = app;
